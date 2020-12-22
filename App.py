c903d800852c3e7ce199016f1f626ad4478074e8
Gold ochim <ochimgold@yahoo.com>
2020-12-22 22:55:03 +0000

    Update App.py

--- App.py
+++ App.py
@@ -16,7 +16,7 @@ def welcome():
 def job_Prediction(Gender, Course, School_Type,Qualification, Job_Search_Mode):
     prediction=clf.predict([['Gender', 'Course', 'School_Type', 'Qualification', 'Job_Search_Mode']])
     print(prediction)
-    return prediction
+    return 'The prediction for job via NYSC is '+ prediction
 
 def main():
     st.title("First Job Predictor")
